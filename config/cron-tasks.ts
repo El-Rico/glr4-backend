8230d6a6c16736cb6dc49f023d75531612594809
Rick Snackers <rsnackers@gmail.com>
2024-12-19 13:45:01 +0100

    Changed cronjobs

--- config/cron-tasks.ts
+++ config/cron-tasks.ts
@@ -11,12 +11,12 @@ export default {
 
       while (
         newDays.getMonth() === currentMonth + 1 &&
-        newDays.getFullYear() === 2024
+        newDays.getFullYear() === 2025
       ) {
         // MAANDAG
         if (newDays.getDay() === 1) {
-          await addLessons(newDays, 12, 0, 1, 6); // Date, hour, minutes, classId, capacity
-          await addLessons(newDays, 15, 0, 2, 6);
+          await addLessons(newDays, 9, 30, 1, 6); // Date, hour, minutes, classId, capacity
+          await addLessons(newDays, 12, 30, 2, 6);
           await addLessons(newDays, 19, 0, 3, 6);
         }
         // DINSDAG
@@ -25,24 +25,25 @@ export default {
         }
         // DONDERDAG
         if (newDays.getDay() === 4) {
-          await addLessons(newDays, 10, 30, 5, 6);
-          await addLessons(newDays, 13, 0, 6, 6);
+          await addLessons(newDays, 9, 30, 5, 6);
+          await addLessons(newDays, 12, 30, 6, 6);
           await addLessons(newDays, 19, 0, 7, 6);
         }
         // VRIJDAG
         if (newDays.getDay() === 5) {
-          await addLessons(newDays, 10, 30, 8, 6);
+          await addLessons(newDays, 9, 30, 8, 6);
+          await addLessons(newDays, 12, 30, 9, 6);
         }
         // ZATERDAG
         if (newDays.getDay() === 6) {
-          await addLessons(newDays, 12, 0, 9, 6);
+          await addLessons(newDays, 10, 0, 10, 6);
         }
 
         newDays.setDate(newDays.getDate() + 1);
       }
     },
     options: {
-      rule: "0 0 1 1 * *",
+      rule: "0 0 15 19 * *",
       tz: "Europe/Amsterdam",
     },
   },
