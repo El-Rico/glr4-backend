23c7e5274bfd04fbf183f9ecb8fe7b2569d07d3d
Rick Snackers <rsnackers@gmail.com>
2026-01-01 22:29:01 +0100

    rerun cron

--- config/cron-tasks.ts
+++ config/cron-tasks.ts
@@ -51,7 +51,7 @@ export default {
       console.log("CRON JOB FINISHED");
     },
     options: {
-      rule: "20 21 1 * *", // At 21:20 on day-of-month 1 // Server time een uur vroeger
+      rule: "35 22 1 * *", // At 22:35 on day-of-month 1 // Server time een uur vroeger
       tz: "Europe/Amsterdam",
     },
   },
